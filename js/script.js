b04a9260618da59405175357867881446da084c7
edavid78 <61856383+edavid78@users.noreply.github.com>
2020-06-05 21:10:33 -0400

    Complete

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function myopenFunction() {
+    document.getElementById("closeMobilenav").style.display = "block";
+    document.getElementById("openMobilenav").style.display = "none";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function mycloseFunction() {
+    document.getElementById("closeMobilenav").style.display = "none";
+    document.getElementById("openMobilenav").style.display = "block";
+    document.getElementById("overlay").style.display = "none";
+}
